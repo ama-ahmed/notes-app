122c960f67e651b8496eb4a98c51d5c8372f2e20
ama-ahmed <ama.ahmed.mohammed@gmail.com>
2022-09-06 00:31:33 +0300

    Adding serverless error logging

--- services/util/handler.js
+++ services/util/handler.js
@@ -1,14 +1,19 @@
+import * as debug from "./debug";
+
 export default function handler(lambda) {
   return async function (event, context) {
     let body, statusCode;
+
+    // Start debugger
+    debug.init(event);
 
     try {
       // Run the Lambda
       body = await lambda(event, context);
       statusCode = 200;
     } catch (e) {
-      // Prints the full error
-      console.error(e);
+      // Print debug messages
+      debug.flush(e);
 
       body = { error: e.message };
       statusCode = 500;
